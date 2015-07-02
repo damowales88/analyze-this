1d518d7d11d4f90649389f2cb9361db8f72b4606
Damowales88 <damien.b.engelhardt@gmail.com>
2015-07-01 21:35:38 -0400

    Created landing pages

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AnalyzeThis::Application.config.secret_key_base = '3ee52e9019a72e003997423a0432af6f5c7b3844db6ff4504d677c1e417309afb8ac743d11c763986a661b95839f43d5bf0e2171fd5e33cf1cf1f21041a43029'
